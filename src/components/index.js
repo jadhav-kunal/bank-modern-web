7c90842fec1af73200cd7aec3a8e98f36abcb6c0
jadhav-kunal <jadhavkunal1999@gmail.com>
2023-02-28 14:58:44 +0530

    Added basic layout to App and Created navbar component

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Stats from "./Stats";
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import CTA from "./CTA";
+import Testimonials from "./Testimonials";
+import Clients from "./Clients";
+import GetStarted from "./GetStarted";
+import Button from "./Button";
+import FeedbackCard from "./FeedbackCard";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Footer,
+  Stats,
+  Billing,
+  Business,
+  CardDeal,
+  CTA,
+  Testimonials,
+  Clients,
+  GetStarted,
+  Button,
+  FeedbackCard,
+  Hero,
+};
